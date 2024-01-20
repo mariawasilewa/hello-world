ab2a95c6f13a1d63aca0929c2017c6f29fbad702
mariawasilewa-coca-cola <marvasileva@coca-cola.com>
2024-01-20 22:45:29 +0200

    Update HelloWorld.spec.js

--- tests/unit/HelloWorld.spec.js
+++ tests/unit/HelloWorld.spec.js
@@ -4,6 +4,7 @@ import HelloWorld from "@/components/HelloWorld.vue";
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "Hello Github";
+    const FAKE_SECRET = '1234567890abcdef';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
